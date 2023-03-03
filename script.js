6da1e1b684b3269d04a6f2b896e952dfa7ca3ce0
Somya <isomya13@gmail.com>
2023-03-04 05:25:58 +0530

    randomizing pieces

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let CANVAS= null;
 let CONTEXT= null;
 let SCALER= 0.9;
 let SIZE= {
-    x:0, y:0, width:0,height:0
+    x:0, y:0, width:0,height:0,
+    rows:3, columns:3
 }
+let PIECES=[];
 
 function main(){
 
@@ -27,7 +29,8 @@ function main(){
         SIZE.height= resizer*VIDEO.videoHeight;
         SIZE.x= window.innerWidth/2 - SIZE.width/2;
         SIZE.y= window.innerHeight/2- SIZE.height/2;
-
+        
+        initializePieces(SIZE.rows, SIZE.columns);
         updateCanvas();
       }
   }).catch((err)=>{
@@ -39,7 +42,68 @@ function main(){
 
 function updateCanvas(){
 //   console.log("hello");
-  
+  CONTEXT.clearRect(0,0,CANVAS.width, CANVAS.height)
+  CONTEXT.globalAlpha=0.5;
   CONTEXT.drawImage(VIDEO,SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+
+//   CONTEXT.globalAlpha=1;
+  for(let i=0; i<PIECES.length; i++){
+    PIECES[i].draw(CONTEXT);
+  }
   window.requestAnimationFrame(updateCanvas);
+
+}
+
+function initializePieces(rows,cols){
+    SIZE.rows= rows;
+    SIZE.columns=cols;
+
+    PIECES=[];
+    for(let i=0; i<SIZE.rows; i++){
+        for(let j=0; j<SIZE.columns
+            ; j++){
+                PIECES.push(new Piece(i,j));
+            }
+    }
+}
+
+function randomizePieces(){
+    for(let i=0; i<PIECES.length; i++){
+          let loc={
+                x: Math.random()*(CANVAS.width - PIECES[i].width),
+                y: Math.random()*(CANVAS.height-PIECES[i].height)
+            }
+
+        PIECES[i].x=loc.x;
+        PIECES[i].y= loc.y;
+    }
+}
+
+class Piece{
+    constructor(rowIndex, colIndex){
+        this.rowIndex=rowIndex;
+        this.colIndex=colIndex;
+        this.x= SIZE.x + SIZE.width*this.colIndex/SIZE.columns;
+        this.y= SIZE.y+ SIZE.height*this.rowIndex/SIZE.rows;
+        this.width= SIZE.width/SIZE.columns;
+        this.height= SIZE.height/SIZE.rows;
+    }
+
+    draw(context){
+
+        context.beginPath();
+
+        context.drawImage(VIDEO,
+            this.colIndex*VIDEO.videoWidth/SIZE.columns,
+            this.rowIndex*VIDEO.videoHeight/SIZE.rows,
+            VIDEO.videoWidth/SIZE.columns,
+            VIDEO.videoHeight/SIZE.rows,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        )
+        context.rect(this.x, this.y, this.width, this.height)
+        context.stroke();
+    }
 }
